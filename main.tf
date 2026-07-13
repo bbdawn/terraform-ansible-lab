1f6a2a36e0a7a55e176f6ea82162d2706b4da59a
이주현 <jh.lee2@okestro.com>
2026-07-13 20:21:42 +0900

    add : terraform으로 vm 생성 후 ansible로 dns 설정, nginx 설치까지

---
+++ main.tf
@@ -0,0 +1,116 @@
+# ## main.tf
+# - 리소스를 정의한다.
+# - variables.tf에서 선언한 변수를 사용하여 인프라를 구성한다.
+# - --> pool-member-1, pool-member-2 생성
+
+terraform {
+  required_providers {
+    openstack = {
+      source = "terraform-provider-openstack/openstack"
+    }
+
+    local = {
+      source = "hashicorp/local"
+    }
+
+    null = {
+      source = "hashicorp/null"
+    }
+
+    time = {
+      source = "hashicorp/time"
+    }
+  }
+}
+
+# vi ~/.config/openstack/clouds.yaml
+provider "openstack" {
+  cloud = "openstack" # ~/.config/openstack/clouds.yaml 참조
+}
+
+# loadbalancer pool member1로 사용할 인스턴스
+resource "openstack_compute_instance_v2" "pool-member-1" {
+  name      = "pool-member-1"
+  flavor_id = var.flavor_id
+  key_pair = var.key_pair
+
+  user_data = <<EOF
+#cloud-config
+runcmd:
+  - sed -i 's/^PubkeyAuthentication no/PubkeyAuthentication yes/' /etc/ssh/sshd_config
+  - systemctl restart ssh
+EOF
+
+  block_device {
+    uuid                  = var.image_id
+    source_type           = "image"
+    destination_type      = "volume"
+    volume_size           = var.volume_size
+    boot_index            = 0
+    delete_on_termination = true
+  }
+
+  network {
+    uuid = var.network_id
+  }
+
+  security_groups = ["default"]
+}
+
+# loadbalancer pool member2로 사용할 인스턴스
+resource "openstack_compute_instance_v2" "pool-member-2" {
+  name      = "pool-member-2"
+  flavor_id = var.flavor_id
+  key_pair = var.key_pair
+
+  user_data = <<EOF
+#cloud-config
+runcmd:
+  - sed -i 's/^PubkeyAuthentication no/PubkeyAuthentication yes/' /etc/ssh/sshd_config
+  - systemctl restart ssh
+EOF
+
+  block_device {
+    uuid                  = var.image_id
+    source_type           = "image"
+    destination_type      = "volume"
+    volume_size           = var.volume_size
+    boot_index            = 0
+    delete_on_termination = true
+  }
+
+  network {
+    uuid = var.network_id
+  }
+
+  security_groups = ["default"]
+}
+
+resource "local_file" "ansible_inventory" {
+
+  filename = "../ansible/inventory.ini"
+
+  content = <<EOF
+[pool_members]
+pool-member-1 ansible_host=${openstack_compute_instance_v2.pool-member-1.access_ip_v4}
+pool-member-2 ansible_host=${openstack_compute_instance_v2.pool-member-2.access_ip_v4}
+
+[pool_members:vars]
+ansible_user=root
+ansible_connection=ssh
+EOF
+
+}
+
+resource "null_resource" "run_ansible" {
+
+  depends_on = [
+    local_file.ansible_inventory
+  ]
+
+  provisioner "local-exec" {
+    working_dir = "../ansible"
+
+    command = "ansible-playbook install-nginx.yml"
+  }
+}
---
+++ outputs.tf
@@ -0,0 +1,33 @@
+# ## outputs.tf
+# - 생성된 리소스의 정보를 출력한다.
+# - 다른 Terraform 설정이나 스크립트(Ansible 등)에서 사용할 값을 정의한다.
+# - --> gpu_ip, qdrant_ip 출력
+
+# output "pool_member_1_ip" {
+#   value       = openstack_compute_instance_v2.pool-member-1.access_ip_v4
+#   description = "pool member 1 IP (Ansible inventory에 사용)"
+# }
+#
+# output "pool_member_2_ip" {
+#   value       = openstack_compute_instance_v2.pool-member-2.access_ip_v4
+#   description = "pool member 2 IP (Ansible inventory에 사용)"
+# }
+#
+# output "pool_member_3_ip" {
+#   value       = openstack_compute_instance_v2.pool-member-3.access_ip_v4
+#   description = "pool member 1 IP (Ansible inventory에 사용)"
+# }
+#
+# output "pool_member_4_ip" {
+#   value       = openstack_compute_instance_v2.pool-member-4.access_ip_v4
+#   description = "pool member 2 IP (Ansible inventory에 사용)"
+# }
+
+output "pool_member_ips" {
+  value = {
+    "pool-member-1" = openstack_compute_instance_v2.pool-member-1.access_ip_v4
+    "pool-member-2" = openstack_compute_instance_v2.pool-member-2.access_ip_v4
+  }
+
+  description = "IP list for ansible inventory"
+}
